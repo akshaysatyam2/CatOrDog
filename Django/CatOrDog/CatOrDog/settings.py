08231044ea8f522e72178c37869fa9642178217c
akshaysatyam2 <akshaysatyam2003@gmail.com>
2023-06-07 18:42:01 +0530

    Working on Cat or Dog Project using Deep Learning(CNN). Changed settings for Django project also added urls, apps, etc.

--- Django/CatOrDog/CatOrDog/settings.py
+++ Django/CatOrDog/CatOrDog/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'CheckCatOrDog.apps.CheckcatordogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
