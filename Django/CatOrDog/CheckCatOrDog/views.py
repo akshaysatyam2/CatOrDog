418cead2c485482266b1c2ec2187e68cf8c0b031
akshaysatyam2 <akshaysatyam2003@gmail.com>
2023-06-08 13:54:52 +0530

    Working on Cat or Dog Project using Deep Learning(CNN). Changed settings for Django project files.

--- Django/CatOrDog/CheckCatOrDog/views.py
+++ Django/CatOrDog/CheckCatOrDog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+import pickle
 
-# Create your views here.
+def home(request):
+    return render(request, 'CheckCatOrDog/index.html')
+
+def getPredictions(Country ,CreditScore,Gender, Age, Tenure,Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary):
+    cnn = pickle.load(open('ml_model.sav', 'rb'))
+
+
+
+def result(request):
+
+    img = (request.POST.get('img'))
+
+
+    # print(f"Fetched {Country }")
+
+    result = getPredictions(img)
+    print(result)
+    return render(request, 'CheckCatOrDog/result.html', {'result': result})
